98beec19a9c2ecdfafc5a85f37e3e81ffc651126
Dmitry Skok <dimas.skok95@gmail.com>
2022-03-11 19:19:27 +0100

    Add react-roter, create new components

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import styles from './NavBar.module.scss';
+import Container from '../Container/Container';
+import 'font-awesome/css/font-awesome.min.css';
+
+const NavBar = () => {
+
+  return (
+    <nav className={styles.navBar}>
+        <div className={styles.navBarWrapper}>
+          <div>
+            <a href='/'>
+              <span className='fa fa-th-list'></span>
+            </a> 
+          </div>
+
+          <div>
+            <a href='/'>Home</a>
+            <a href='/about'>About</a>
+            <a href='/favorite'>Favorite</a>
+          </div>
+        </div>
+    </nav>
+  );
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store'
+import { BrowserRouter } from 'react-router-dom';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.querySelector('#root')
 );
